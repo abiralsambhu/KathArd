c9f140f56660ade1f98c9f5458299b975098717e
Shiva Gaire <abiralsambhu@gmail.com>
2015-12-13 15:10:12 +0545

    FUN project

---
+++ AndroidStudioProjects/KathArd/app/src/main/java/shiva/kathard/MyActivity.java
@@ -0,0 +1,33 @@
+package shiva.kathard;
+
+/**
+ * Created by shiva on 12/13/15.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by hereshem on 11/28/15.
+ */
+public class MyActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_layout);
+
+        Button btn_forward = (Button) findViewById(R.id.btn_forward);
+
+        btn_forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Forward button clicked"
+                        , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
